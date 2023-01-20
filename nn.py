e5d8c759dd9118d192b7773a79d216c5bfd23815
hosnahoseini <hosna.hoseini@gmail.com>
2023-01-20 16:57:48 +0330

    [feat] NN implementation

--- nn.py
+++ nn.py
@@ -7,15 +7,24 @@ class NeuralNetwork():
 
         # TODO
         # layer_sizes example: [4, 10, 2]
-        pass
+        self.W1 = np.random.normal(layer_sizes[0], layer_sizes[1])
+        self.W2 = np.random.normal(layer_sizes[1], layer_sizes[2])
+
+        self.b1 = np.zeros((layer_sizes[0], 1))
+        self.b1 = np.zeros((layer_sizes[1], 1))
 
     def activation(self, x):
         
         # TODO
-        return x
+        return 1 / (1 + np.exp(x))
 
     def forward(self, x):
         
         # TODO
         # x example: np.array([[0.1], [0.2], [0.3]])
-        pass
+        A1 = self.W1 @ x + self.b1
+        Z1 = self.activation(A1)
+        A2 = self.W2 @ Z1 + self.b2
+        Z2 = self.activation(A2)
+
+        return Z2
